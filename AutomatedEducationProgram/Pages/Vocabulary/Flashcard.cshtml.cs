1ddf1c2567ac5fa20072d27b9a5006fa9b5bf419
jordanmartell0 <jordanmartell0@gmail.com>
2023-11-15 16:10:02 -0700

    Added very basic flashcard page and functionality (proof of concept)

---
+++ AutomatedEducationProgram/Pages/Vocabulary/Flashcard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutomatedEducationProgram.Pages.Vocabulary
+{
+    public class FlashcardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AutomatedEducationProgram/wwwroot/js/Flashcard.js
@@ -0,0 +1,5 @@
+﻿function flip() {
+    var elem = document.getElementById("flashCard");
+    if (elem.innerHTML == "Function") elem.innerHTML = "This is the definition of a function";
+    else elem.innerHTML = "Function";
+}
