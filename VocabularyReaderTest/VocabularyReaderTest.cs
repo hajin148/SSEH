7084db1cd98ca59489a741a0c16257444ac2986f
jordanmartell0 <jordanmartell0@gmail.com>
2023-12-04 16:06:31 -0700

    Updated unit tests and added more parsing tests.

--- VocabularyReaderTest/VocabularyReaderTest.cs
+++ VocabularyReaderTest/VocabularyReaderTest.cs
@@ -17,7 +17,7 @@
         }
 
         [TestMethod]
-        public void ParseTermsAndDefs()
+        public void ParseTermsAndDefsPhilos()
         {
             string chatOutput = File.ReadAllText("..\\..\\..\\TestFiles\\SampleChatGPTOutput.txt");
             List<VocabularyWord> words = VocabularyReader.ParseTermsAndDefs(chatOutput, "[[[", "]]]");
@@ -62,6 +62,78 @@
             string expected = "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc\nabcabcabcabcabcabcabcabcabcabcabcabcabc";
             string actual = VocabularyReader.ReadWordsFromFile("..\\..\\..\\TestFiles\\Sample3.pdf");
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ParseTermsAndDefsMath()
+        {
+            string chatOutput = File.ReadAllText("..\\..\\..\\TestFiles\\SampleChatGPTOutput2.txt");
+            List<VocabularyWord> words = VocabularyReader.ParseTermsAndDefs(chatOutput, "[[[", "]]]");
+            Assert.AreEqual("Function:", words[0].Term);
+            Assert.AreEqual("Domain:", words[1].Term);
+            Assert.AreEqual("Range:", words[2].Term);
+            Assert.AreEqual("Symmetry:", words[3].Term);
+            Assert.AreEqual("Vertical Line Test:", words[4].Term);
+            Assert.IsTrue(words[0].Definition.Contains("A function is a special type of relation between two sets, A and B, where each element in the first"));
+            Assert.IsTrue(words[1].Definition.Contains("of all possible values for the independent variable."));
+            Assert.IsTrue(words[2].Definition.Contains("consisting of all possible values for the dependent variable. It represents"));
+            Assert.IsTrue(words[3].Definition.Contains("Symmetry properties of a function describe its behavior in relation to certain transformations."));
+            Assert.IsTrue(words[4].Definition.Contains("It states that every vertical line drawn should intersect the graph"));
+            Assert.IsTrue(words.Count == 5);
+        }
+
+        [TestMethod]
+        public void ParseTermsAndDefsEnglish()
+        {
+            string chatOutput = File.ReadAllText("..\\..\\..\\TestFiles\\SampleChatGPTOutput3.txt");
+            List<VocabularyWord> words = VocabularyReader.ParseTermsAndDefs(chatOutput, "[[[", "]]]");
+            Assert.AreEqual("Sociohistorical Context:", words[0].Term);
+            Assert.AreEqual("Marvel Cinematic Universe (MCU):", words[1].Term);
+            Assert.AreEqual("Offensive Language:", words[2].Term);
+            Assert.AreEqual("#OscarsSoWhite:", words[3].Term);
+            Assert.AreEqual("Diversity in Hollywood:", words[4].Term);
+            Assert.IsTrue(words[0].Definition.Contains("The cultural and historical background that influences"));
+            Assert.IsTrue(words[1].Definition.Contains("franchise and shared universe that includes a series of superhero films"));
+            Assert.IsTrue(words[2].Definition.Contains("inappropriate, or harmful, particularly in the context of artistic works"));
+            Assert.IsTrue(words[3].Definition.Contains("media hashtag and movement that emerged to protest the lack of diversity, particularly the underrepresentation"));
+            Assert.IsTrue(words[4].Definition.Contains("a variety of voices, perspectives, and representations, particularly concerning race,"));
+            Assert.IsTrue(words.Count == 5);
+        }
+
+        [TestMethod]
+        public void ParseTermsAndDefsHistory()
+        {
+            string chatOutput = File.ReadAllText("..\\..\\..\\TestFiles\\SampleChatGPTOutput4.txt");
+            List<VocabularyWord> words = VocabularyReader.ParseTermsAndDefs(chatOutput, "[[[", "]]]");
+            Assert.AreEqual("Middle Ages:", words[0].Term);
+            Assert.AreEqual("Black Death:", words[1].Term);
+            Assert.AreEqual("Feudal Society:", words[2].Term);
+            Assert.AreEqual("Great Schism:", words[3].Term);
+            Assert.AreEqual("Reconquista:", words[4].Term);
+            Assert.IsTrue(words[0].Definition.Contains("The historical period between the fall of the Roman Empire"));
+            Assert.IsTrue(words[1].Definition.Contains("A devastating pandemic that struck Europe in the 1340s, caused by the bubonic plague"));
+            Assert.IsTrue(words[2].Definition.Contains("A social and economic system in medieval Europe where lords owned land, knights provided military service"));
+            Assert.IsTrue(words[3].Definition.Contains("The split in the Christian Church in 1054, dividing it into the western Roman Catholic Church"));
+            Assert.IsTrue(words[4].Definition.Contains("Christian-Muslim conflicts during the Crusades for control of the Holy Land."));
+            Assert.IsTrue(words.Count == 5);
+        }
+
+        [TestMethod]
+        public void ParseTermsAndDefsScience()
+        {
+            string chatOutput = File.ReadAllText("..\\..\\..\\TestFiles\\SampleChatGPTOutput5.txt");
+            List<VocabularyWord> words = VocabularyReader.ParseTermsAndDefs(chatOutput, "[[[", "]]]");
+            Assert.AreEqual("Dalton's Atomic Theory:", words[0].Term);
+            Assert.AreEqual("Atomos:", words[1].Term);
+            Assert.AreEqual("Law of Definite Proportions:", words[2].Term);
+            Assert.AreEqual("Law of Multiple Proportions:", words[3].Term);
+            Assert.AreEqual("Isooctane:", words[4].Term);
+            Assert.IsTrue(words[0].Definition.Contains("A scientific theory proposed by John Dalton in 1807, consisting of the following postulates:"));
+            Assert.IsTrue(words[1].Definition.Contains("A term used by ancient Greek philosophers Leucippus and Democritus in the fifth century"));
+            Assert.IsTrue(words[2].Definition.Contains("it states that all samples"));
+            Assert.IsTrue(words[3].Definition.Contains("States that when two elements react to form more than one compound, a fixed mass of one element"));
+            Assert.IsTrue(words[4].Definition.Contains("mentioned in the text as an example illustrating the constant composition of compounds. It has a carbon-to-hydrogen mass ratio of 5.33:1."));
+            Assert.IsTrue(words.Count == 5);
         }
     }
 }
