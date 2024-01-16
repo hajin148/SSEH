946f68078fc8f78eb60f4e8b5d0f32ca1abaf271
jordanmartell0 <jordanmartell0@gmail.com>
2024-01-16 16:12:01 -0700

    Begin work on MyNotes page list of notes.

---
+++ AutomatedEducationProgram/Pages/MyNotes/MyNotes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutomatedEducationProgram.Pages.MyNotes
+{
+    public class MyNotesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
