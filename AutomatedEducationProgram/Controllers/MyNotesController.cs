50ae2b805fda7b70e115956ab25bfe7821729995
jordanmartell0 <jordanmartell0@gmail.com>
2024-01-19 11:29:13 -0700

    Working on edit note page

--- AutomatedEducationProgram/Controllers/MyNotesController.cs
+++ AutomatedEducationProgram/Controllers/MyNotesController.cs
@@ -6,6 +6,7 @@ using EduApp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Utilities;
 
 namespace AutomatedEducationProgram.Controllers
 {
@@ -63,6 +64,63 @@ namespace AutomatedEducationProgram.Controllers
         [HttpPost]
         public async Task<IActionResult> EditNote(IFormCollection inputs)
         {
+            int noteId = int.Parse(inputs["noteId"]);
+            List<VocabularyWord> existingVocab = await _context.VocabularyWords.Where(word => word.ParentNote.Id == noteId).ToListAsync();
+            foreach(var word in existingVocab)
+            {
+                _context.VocabularyWords.Remove(word);
+            }
+            List<ExamQuestion> existingQs = await _context.ExamQuestions.Where(q => q.ParentNote.Id == noteId).ToListAsync();
+            foreach (var q in existingQs)
+            {
+                _context.ExamQuestions.Remove(q);
+            }
+            Note toDelete = (await _context.Notes.Where(note => note.Id == noteId).ToListAsync())[0];
+            _context.Remove(toDelete);
+            _context.SaveChanges();
+
+            Note editedNote = new Note();
+            editedNote.VocabularyWords = new List<VocabularyWord>();
+            editedNote.ExamQuestions = new List<ExamQuestion>();
+            foreach (var key in inputs.Keys)
+            {
+                if (key.StartsWith("title"))
+                {
+                    editedNote.Title = inputs[key];
+                } else if (key.StartsWith("description"))
+                {
+                    editedNote.Description = inputs[key];
+                } else if (key.StartsWith("userId"))
+                {
+                    editedNote.UserId = inputs[key];
+                } else if (key.StartsWith("vocabTerm"))
+                {
+                    VocabularyWord word = new VocabularyWord();
+                    word.ParentNote = editedNote;
+                    word.Term = inputs[key];
+                    string defKey = key.Replace("Term", "Def");
+                    word.Definition = inputs[defKey];
+                    editedNote.VocabularyWords.Add(word);
+                } else if (key.StartsWith("question"))
+                {
+                    ExamQuestion q = new ExamQuestion();
+                    q.ParentNote = editedNote;
+                    q.Question = inputs[key];
+                    string aKey = key.Replace("question", "ansA");
+                    q.AnswerA = inputs[aKey];
+                    string bKey = key.Replace("question", "ansB");
+                    q.AnswerB = inputs[bKey];
+                    string cKey = key.Replace("question", "ansC");
+                    q.AnswerC = inputs[cKey];
+                    string dKey = key.Replace("question", "ansD");
+                    q.AnswerD = inputs[dKey];
+                    string eKey = key.Replace("question", "explanation");
+                    q.Explanation = inputs[eKey];
+                    editedNote.ExamQuestions.Add(q);
+                }
+            }
+            _context.Notes.Add(editedNote);
+            _context.SaveChanges();
             return View();
         }
     }
