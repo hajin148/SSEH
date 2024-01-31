a6cb8e9bdf5c5cc1019b27e4dc9e29a9441ebb25
jordanmartell0 <jordanmartell0@gmail.com>
2024-01-30 21:13:48 -0700

    Added migration for changes to ExamQuestion model. Updated link to MyNotes

--- AutomatedEducationProgram/Migrations/AutomatedEducationProgram/20240131040959_Updated Exam Q Model.Designer.cs
+++ AutomatedEducationProgram/Migrations/AutomatedEducationProgram/20240131040959_Updated Exam Q Model.Designer.cs
@@ -4,6 +4,7 @@ using AutomatedEducationProgram.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AutomatedEducationProgram.Migrations.AutomatedEducationProgram
 {
     [DbContext(typeof(AutomatedEducationProgramContext))]
-    partial class AutomatedEducationProgramContextModelSnapshot : ModelSnapshot
+    [Migration("20240131040959_Updated Exam Q Model")]
+    partial class UpdatedExamQModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AutomatedEducationProgram/Migrations/AutomatedEducationProgram/20240131040959_Updated Exam Q Model.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutomatedEducationProgram.Migrations.AutomatedEducationProgram
+{
+    public partial class UpdatedExamQModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Answer",
+                table: "ExamQuestions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Answer",
+                table: "ExamQuestions");
+        }
+    }
+}
