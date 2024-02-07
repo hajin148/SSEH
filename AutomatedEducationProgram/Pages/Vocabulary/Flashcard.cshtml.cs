eaf30109d6e1fb7632ffea6a3af1d6f29d626053
Hajin_Jeon <u1437827@umail.utah.edu>
2024-02-06 21:13:39 -0700

    Flashcard implementation version with loop use.
    It needs to be further implemented.
    
    //TODO
    receive vocabularies of note from User, and NoteID.
    Then place those vocabularies into ProcessedVocabulary variable.

--- AutomatedEducationProgram/Pages/Vocabulary/Flashcard.cshtml.cs
+++ AutomatedEducationProgram/Pages/Vocabulary/Flashcard.cshtml.cs
@@ -1,4 +1,5 @@
 using AutomatedEducationProgram.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -8,14 +9,31 @@ namespace AutomatedEducationProgram.Pages.Vocabulary
     public class FlashcardModel : PageModel
     {
         public List<VocabularyWord> ProcessedVocabulary { get; set; }
+        public string text { get; set; }
 
         public void OnGet()
         {
             var vocabularyJson = HttpContext.Session.GetString("ProcessedVocabulary");
+            var textJson = HttpContext.Session.GetString("Text");
             if (!string.IsNullOrEmpty(vocabularyJson))
             {
                 ProcessedVocabulary = JsonConvert.DeserializeObject<List<VocabularyWord>>(vocabularyJson);
+                text = JsonConvert.DeserializeObject<string>(textJson);
             }
+
+            // ProcessedVocabulary should be feed up with the received Vocab from the db.
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            var vocabularyJson = JsonConvert.SerializeObject(ProcessedVocabulary);
+            HttpContext.Session.SetString("ProcessedVocabulary", vocabularyJson);
+
+            var textJson = JsonConvert.SerializeObject(text);
+            HttpContext.Session.SetString("Text", textJson);
+
+            return Page();
         }
     }
 }
+
