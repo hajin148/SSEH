7f1132d6f5187498b3869bcd0f90be6bb81a6899
jordanmartell0 <jordanmartell0@gmail.com>
2024-02-12 13:11:49 -0700

    Working on dynamic addition/removal of content on EditNote page.

--- AutomatedEducationProgram/Pages/MyNotes/EditNote.cshtml.cs
+++ AutomatedEducationProgram/Pages/MyNotes/EditNote.cshtml.cs
@@ -44,8 +44,27 @@ namespace AutomatedEducationProgram.Pages.MyNotes
         {
             int noteId = int.Parse(inputs["noteId"]);
             Note editedNote = (_context.Notes.Where(note => note.Id == noteId).FirstOrDefault());
-            editedNote.VocabularyWords = new List<VocabularyWord>();
-            editedNote.ExamQuestions = new List<ExamQuestion>();
+            DocumentText defaultDoc = null;
+            Dictionary<int, bool> foundTermIds = new Dictionary<int, bool>();
+            Dictionary<int, bool> foundQIds = new Dictionary<int, bool>();
+            // Determine the default documentText for any new content added by the user
+            IEnumerable<VocabularyWord> existingVocab = _context.VocabularyWords.Where(word => word.ParentNote.Id == noteId).ToList();
+            IEnumerable<ExamQuestion> existingQ = _context.ExamQuestions.Where(q => q.ParentNote.Id == noteId).ToList();
+            if (existingVocab.FirstOrDefault() != null)
+            {
+                defaultDoc = existingVocab.FirstOrDefault().RelevantDoc;
+            } else
+            {
+                if (existingQ.FirstOrDefault() != null)
+                {
+                    defaultDoc = existingQ.FirstOrDefault().RelevantDoc;
+                }
+            }
+
+            // I think these can be removed
+            //editedNote.VocabularyWords = new List<VocabularyWord>();
+            //editedNote.ExamQuestions = new List<ExamQuestion>();
+
             foreach (var key in inputs.Keys)
             {
                 if (key.StartsWith("title"))
@@ -59,15 +78,27 @@ namespace AutomatedEducationProgram.Pages.MyNotes
                 else if (key.StartsWith("vocabTerm"))
                 {
                     int id = int.Parse(key.Split(" ")[1]);
+                    foundTermIds.Add(id, true);
                     VocabularyWord word = _context.VocabularyWords.Where(w => w.ID == id).FirstOrDefault();
                     word.Term = inputs[key];
                     string defKey = key.Replace("Term", "Def");
                     word.Definition = inputs[defKey];
                     _context.VocabularyWords.Update(word);
                 }
+                else if (key.StartsWith("newVocabTerm"))
+                {
+                    VocabularyWord newTerm = new VocabularyWord();
+                    newTerm.Term = inputs[key];
+                    string defKey = key.Replace("Term", "Def");
+                    newTerm.Definition = inputs[defKey];
+                    newTerm.ParentNote = editedNote;
+                    newTerm.RelevantDoc = defaultDoc;
+                    _context.VocabularyWords.Add(newTerm);
+                }
                 else if (key.StartsWith("question"))
                 {
                     int id = int.Parse(key.Split(" ")[1]);
+                    foundQIds.Add(id, true);
                     ExamQuestion q = _context.ExamQuestions.Where(question => question.Id == id).FirstOrDefault();
                     q.Question = inputs[key];
                     /*string aKey = key.Replace("question", "ansA");
@@ -89,7 +120,33 @@ namespace AutomatedEducationProgram.Pages.MyNotes
                     q.RelevantDoc = _context.DocumentTexts.Where(dt => dt.Id == docId).FirstOrDefault();*/
                     _context.ExamQuestions.Update(q);
                 }
+                else if (key.StartsWith("newQuestion"))
+                {
+                    ExamQuestion newQ = new ExamQuestion();
+                    newQ.Question = inputs[key];
+                    string ansKey = key.Replace("Question", "GenericAns");
+                    newQ.Answer = inputs[ansKey];
+                    newQ.ParentNote = editedNote;
+                    newQ.RelevantDoc = defaultDoc;
+                    _context.ExamQuestions.Add(newQ);
+                }
             }
+            // Delete any content that didn't get passed by the form
+            foreach (VocabularyWord word in existingVocab)
+            {
+                if (!foundTermIds.ContainsKey(word.ID))
+                {
+                    _context.VocabularyWords.Remove(word);
+                }
+            }
+            foreach (ExamQuestion q in existingQ)
+            {
+                if (!foundQIds.ContainsKey(q.Id))
+                {
+                    _context.ExamQuestions.Remove(q);
+                }
+            }
+
             editedNote.CreatedDate = DateTime.Now;
             _context.Notes.Update(editedNote);
             _context.SaveChanges();
