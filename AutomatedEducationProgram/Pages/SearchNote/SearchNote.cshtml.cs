cf9416ed4095c262266b2d11f5977fa44b32b3e5
AbhiveerSharma <u1275782@utah.edu>
2024-02-23 15:35:16 -0700

    -Implemented the changed UI of "Search For Notes" webpage. - Implemented functionality but the user ID still needs to be fixed. -Search For Users needs to be implemented.

--- AutomatedEducationProgram/Pages/SearchNote/SearchNote.cshtml.cs
+++ AutomatedEducationProgram/Pages/SearchNote/SearchNote.cshtml.cs
@@ -1,12 +1,43 @@
+using AutomatedEducationProgram.Data;
+using AutomatedEducationProgram.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AutomatedEducationProgram.Pages.SearchNote
 {
     public class SearchNoteModel : PageModel
     {
-        public void OnGet()
+        private readonly AutomatedEducationProgramContext _context;
+
+        public SearchNoteModel(AutomatedEducationProgramContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string SearchString { get; set; }
+
+        public List<Note> SearchResults { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                // Retrieve notes that match the search input in either title or description
+                SearchResults = _context.Notes
+                    .Where(note => EF.Functions.Like(note.Title, $"%{SearchString}%") || EF.Functions.Like(note.Description, $"%{SearchString}%"))
+                    .ToList();
+            }
+
+            return Page();
         }
     }
 }
