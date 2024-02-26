e95acfa3abe62e0fbf42b6b810d38cf4673a8625
jordanmartell0 <jordanmartell0@gmail.com>
2024-02-26 11:33:24 -0700

    Bug fix with accessing User info on search note screen

--- AutomatedEducationProgram/Pages/SearchNote/SearchNote.cshtml.cs
+++ AutomatedEducationProgram/Pages/SearchNote/SearchNote.cshtml.cs
@@ -1,5 +1,7 @@
+using AutomatedEducationProgram.Areas.Data;
 using AutomatedEducationProgram.Data;
 using AutomatedEducationProgram.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,30 +13,42 @@ namespace AutomatedEducationProgram.Pages.SearchNote
     public class SearchNoteModel : PageModel
     {
         private readonly AutomatedEducationProgramContext _context;
+        private readonly UserManager<AEPUser> _userManager;
 
-        public SearchNoteModel(AutomatedEducationProgramContext context)
+
+
+        public SearchNoteModel(AutomatedEducationProgramContext context, UserManager<AEPUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [BindProperty]
         public string SearchString { get; set; }
 
         public List<Note> SearchResults { get; set; }
+        public List<AEPUser> RelatedUsers { get; set; }
 
         public IActionResult OnGet()
         {
             return Page();
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
+            RelatedUsers = new List<AEPUser>();
             if (!string.IsNullOrEmpty(SearchString))
             {
-                // Retrieve notes that match the search input in either title or description
+                // Retrieve notes that match the search input in either title or description,
+                // including the related user data
                 SearchResults = _context.Notes
-                    .Where(note => EF.Functions.Like(note.Title, $"%{SearchString}%") || EF.Functions.Like(note.Description, $"%{SearchString}%"))
+                    .Where(note => EF.Functions.Like(note.Title, $"%{SearchString}%") || EF.Functions.Like(note.Description, $"%{SearchString}%")) // Include related user data
                     .ToList();
+                foreach (var note in SearchResults)
+                {
+                    AEPUser relatedUser = await _userManager.FindByIdAsync(note.UserId);
+                    RelatedUsers.Add(relatedUser);
+                }
             }
 
             return Page();
