84c87ee749870bc168f5aa418a9cf5b90ce6efff
Hajin_Jeon <u1437827@umail.utah.edu>
2024-03-08 16:57:05 -0700

    In progress. Answer checking is not working correctly at this point.

--- AutomatedEducationProgram/Pages/Exam/ResultExam.cshtml.cs
+++ AutomatedEducationProgram/Pages/Exam/ResultExam.cshtml.cs
@@ -1,6 +1,12 @@
+using AutomatedEducationProgram.Areas.Data;
+using AutomatedEducationProgram.Data;
+using AutomatedEducationProgram.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Utilities;
 
 namespace AutomatedEducationProgram.Pages.Exam
 {
@@ -9,19 +15,127 @@ namespace AutomatedEducationProgram.Pages.Exam
         [BindProperty]
         public string UserAnswers { get; set; }
         public Dictionary<int, string> UserAnswersObj { get; set; }
+        public Note CurrentNote { get; set; }
+        public List<ExamQuestion> Questions { get; set; }
+        public DocumentText doc { get; set; }
+        private readonly AutomatedEducationProgramContext _context;
+        private readonly UserManager<AEPUser> _userManager;
+        private readonly IConfiguration _configuration;
+        [BindProperty]
+        public string? noteIdString { get; set; }
+        [BindProperty]
+        public string? numberMCQString { get; set; }
+        [BindProperty]
+        public string? numberShortString { get; set; }
+        [BindProperty]
+        public string? numberTFString { get; set; }
+        [BindProperty]
+        public int? noteId { get; set; }
+        [BindProperty]
+        public int? numberMCQ { get; set; }
+        [BindProperty]
+        public int? numberShort { get; set; }
+        [BindProperty]
+        public int? numberTF { get; set; }
 
-        public void OnPost()
+        public int? totalNumberQuestion { get; set; }
+        public int correctAnswers = 0;
+        public string userId { get; set; }
+
+        public ResultExamModel(AutomatedEducationProgramContext context, UserManager<AEPUser> userManager, IConfiguration configuration)
         {
+            _context = context;
+            _userManager = userManager;
+            _configuration = configuration;
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            noteIdString = JsonConvert.DeserializeObject<string>(noteIdString);
+            noteId = int.Parse(noteIdString);
+
+            numberMCQString = JsonConvert.DeserializeObject<string>(numberMCQString);
+            numberShortString = JsonConvert.DeserializeObject<string>(numberShortString);
+            numberTFString = JsonConvert.DeserializeObject<string>(numberTFString);
+
+            numberMCQ = int.Parse(numberMCQString);
+            numberShort = int.Parse(numberShortString);
+            numberTF = int.Parse(numberTFString);
+
+            totalNumberQuestion = numberMCQ + numberShort + numberTF;
+
             if (!string.IsNullOrWhiteSpace(UserAnswers))
             {
                 UserAnswersObj = JsonConvert.DeserializeObject<Dictionary<int, string>>(UserAnswers);
+                
             }
             else
             {
                 UserAnswersObj = new Dictionary<int, string>();
             }
+
+           
+
+           
+
+            string user = _userManager.GetUserId(User);
+            if (user == null)
+            {
+                return Redirect("https://localhost:7039/Identity/Account/Login");
+            }
+            CurrentNote = _context.Notes.Where(note => note.Id == noteId).FirstOrDefault();
+            Questions = _context.ExamQuestions.Where(q => q.ParentNote.Id == noteId).ToList();
+            userId = _userManager.GetUserId(User);
+
+            foreach (var answer in UserAnswersObj)
+            {
+                var questionNum = answer.Key;
+                var userAnswer = answer.Value;
+
+                CheckMCQAnswers(Questions, questionNum, userAnswer);
+            }
+
+
+            return Page();
+        }
+
+        public void CheckMCQAnswers(List<ExamQuestion> eq, int questionNum, string userAnswer)
+        {
+            var question = eq.FirstOrDefault(q => q.Id == questionNum);
+            if (question != null)
+            {
+                string chosenAnswer = ConvertUserAnswerToActualAnswer(question, userAnswer);
+
+                if (chosenAnswer != null && chosenAnswer.Equals(question.Answer, StringComparison.OrdinalIgnoreCase))
+                {
+                    correctAnswers++;
+                }
+            }
         }
 
 
+        private string ConvertUserAnswerToActualAnswer(ExamQuestion question, string userAnswer)
+        {
+            switch (userAnswer)
+            {
+                case "1":
+                    return question.AnswerA;
+                case "2":
+                    return question.AnswerB;
+                case "3":
+                    return question.AnswerC;
+                case "4":
+                    return question.AnswerD;
+                default:
+                    return null; 
+            }
+        }
+
     }
+
+    
+
 }
+
+
