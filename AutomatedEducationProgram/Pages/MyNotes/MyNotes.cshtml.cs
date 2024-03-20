cb00b8173bfeb5cbf2e3c9683dc2ece44224f123
jordanmartell0 <jordanmartell0@gmail.com>
2024-03-20 11:16:10 -0600

    Basic UI for pending follow request on MyNotes. Basic follower functionality

--- AutomatedEducationProgram/Pages/MyNotes/MyNotes.cshtml.cs
+++ AutomatedEducationProgram/Pages/MyNotes/MyNotes.cshtml.cs
@@ -13,6 +13,9 @@ namespace AutomatedEducationProgram.Pages.MyNotes
         public IEnumerable<Note> UserNotes { get; set; }
         public IEnumerable<DocumentText> UserDocs { get; set; }
 
+        public List<Following> PendingFollowRequests { get; set; }
+        public List<string> FollowRequesterNames {  get; set; }
+
         private readonly AutomatedEducationProgramContext _context;
         private readonly UserManager<AEPUser> _userManager;
         private readonly IConfiguration _configuration;
@@ -34,8 +37,30 @@ namespace AutomatedEducationProgram.Pages.MyNotes
             string userId = _userManager.GetUserId(User);
             UserNotes = _context.Notes.Where(note => note.UserId == userId).ToList();
             UserDocs = _context.DocumentTexts.Where(dtext => dtext.parentNote.UserId == userId).ToList();
-
+            PendingFollowRequests = _context.Followings.Where(f => f.Followed == user && f.Pending).Take(50).ToList();
+            FollowRequesterNames = new List<string>();
+            foreach (var f in PendingFollowRequests)
+            {
+                FollowRequesterNames.Add(_userManager.Users.Where(user => user.Id == f.Follower).First().UserID);
+            }
             return Page();
+        }
+
+        public IActionResult OnPost(IFormCollection inputs)
+        {
+            string action = inputs["action"];
+            int followingId = int.Parse(inputs["followId"]);
+            Following following = _context.Followings.Where(f => f.Id == followingId).FirstOrDefault();
+            if (action == "Approve")
+            {
+                following.Pending = false;
+                _context.Followings.Update(following);
+            } else if (action == "Delete")
+            {
+                _context.Followings.Remove(following);
+            }
+            _context.SaveChanges();
+            return Redirect("MyNotes");
         }
     }
 }
